c551e72da30725f9dd50150faf1c8bc4a7dc16c8
saurabh <saurabhkedar@gmail.com>
2022-10-12 20:17:24 +0530

    fixed the types issue by just using Any

--- src/Pages/Flowstats.tsx
+++ src/Pages/Flowstats.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ZipContext } from '../Zipply'
 
 const Flowstats = () => {
+  const { text, setText, flowbar, setFlowbars } = useContext(ZipContext)
+
+  const texton = (e: any) => {
+    setText(e.target.value)
+  }
+
+  const saveFlow = () => {
+    setFlowbars([...flowbar, text])
+  }
+
   return (
     <div className='flowstats'>
       <div className='btx'>
         <button className='delete'>Delete</button>
         <button className='cancel'>Cancel</button>
-        <button className='Save'>Save</button>
+        <button className='Save' onClick={saveFlow}>
+          Save
+        </button>
       </div>
       <div className='flowinfo'>
         <h2>Flow Info</h2>
         <div className='flowbox'>
           <label>Name</label>
-          <input type='text' placeholder='Enter a name' />
+          <input
+            type='text'
+            placeholder='Enter a name'
+            onChange={(e) => texton(e)}
+          />
         </div>
       </div>
     </div>
--- src/Zipply.tsx
+++ src/Zipply.tsx
@@ -1,41 +1,57 @@
-import React, { useState } from 'react'
+import React, { createContext, useState } from 'react'
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import Blank from './Pages/Blank'
 import Flowstats from './Pages/Flowstats'
 
-const Zipply: React.FC = () => {
-  const [flowbar, setFlowbars] = useState<String[]>([])
+export type Info = {
+  text?: any
+  setText?: any
+
+  flowbar?: any
+  setFlowbars?: any
+}
+
+type ContextProp = {
+  children: React.ReactNode
+}
+
+export const ZipContext: React.Context<any> = createContext('')
+
+const Zipply: React.FC<Info> = () => {
+  const [text, setText] = useState('newFlow')
+  const [flowbar, setFlowbars] = useState([''])
 
   const addNew = () => {
     if (!flowbar.includes('newFlow')) {
-      setFlowbars([...flowbar, 'newFlow'])
+      setFlowbars([...flowbar, text])
     }
   }
   return (
-    <Router>
-      <div className='Zipply'>
-        <div className='sidebar'>
-          <button className='add' onClick={addNew}>
-            Add New Flow
-          </button>
+    <ZipContext.Provider value={{ text, setText, flowbar, setFlowbars }}>
+      <Router>
+        <div className='Zipply'>
+          <div className='sidebar'>
+            <button className='add' onClick={addNew}>
+              Add New Flow
+            </button>
 
-          <div className='flows'>
-            <div className='flow'>Order</div>
-            <div className='flow'>Outage</div>
-
-            {flowbar.map((flow, index) => (
-              <Link to='/flowstats' className='flow' key={index}>
-                {flow}
-              </Link>
-            ))}
+            <div className='flows'>
+              <div className='flow'>Order</div>
+              <div className='flow'>Outage</div>
+              {flowbar.map((flow, index) => (
+                <Link to='/flowstats' className='flow' key={index}>
+                  {flow}
+                </Link>
+              ))}
+            </div>
           </div>
+          <Routes>
+            <Route path='/flowstats' element={<Flowstats />} />
+            <Route path='/empty' element={<Blank />} />
+          </Routes>
         </div>
-        <Routes>
-          <Route path='/flowstats' element={<Flowstats />} />
-          <Route path='/empty' element={<Blank />} />
-        </Routes>
-      </div>
-    </Router>
+      </Router>
+    </ZipContext.Provider>
   )
 }
 
